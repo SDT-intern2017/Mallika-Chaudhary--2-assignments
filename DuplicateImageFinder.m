89797ea601e48521cd45a7f6225c0c56acc3ced1
SDT-intern2017 <sdtintern2017@gmail.com>
2017-07-20 11:18:24 +0400

    Update DuplicateImageFinder.m

--- DuplicateImageFinder.m
+++ DuplicateImageFinder.m
@@ -1,21 +1,23 @@
-% Program to find duplicate images 
-
-query_image=input('Enter the query image');
-qi=rgb2gray(imread(query_image));
+% Program to find duplicate image
+qi=rgb2gray(imread('brick1.png'));
 lbpQuery=extractLBPFeatures(qi,'Upright',false);
-file_location=input('Enter the location where you want to check the duplicate images');
-imagefiles=dir('*.jpg');
+%file_location=input('Enter the location where you want to check the duplicate images');
+imagefiles=dir('*.png');
 number_of_images=length(imagefiles);
-feature=zeros(number_of_images,2);
+feature=zeros(number_of_images,1);
 for i=1:number_of_images
     currentimage=imagefiles(i).name;
     current=rgb2gray(imread(currentimage));
     lbpCurrent=extractLBPFeatures(current,'Upright',false);
-    feature(i,1)=sum((lbpCurrent - lbpQuery).^2);
-    feature(i,2)=currentimage;
+    e_distance=sum((lbpCurrent - lbpQuery).^2);
+    feature(i,1)=e_distance;
+    feature(i,2)=i;
 end
 sorted=sortrows(feature,1);
+m=feature(1,2);
+k=imagefiles(m,1);
+j=k.name;
 figure
-imshow(feature(1,2));
+imshow(j);
 title('Duplicate image is');
     
